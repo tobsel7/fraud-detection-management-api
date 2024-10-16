db2ee6a3b8e6cfa0c99b204f03314585acc6b6f1
Tobias Haider <tobias.haider@extern.dkbcodefactory.com>
2024-10-16 22:51:47 +0200

    Add testcontainers for integration tests

--- src/test/java/com/tobsel/api/fraud/FraudDetectionManagementApiApplicationTests.java
+++ src/test/java/com/tobsel/api/fraud/FraudDetectionManagementApiApplicationTests.java
@@ -2,8 +2,12 @@ package com.tobsel.api.fraud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class FraudDetectionManagementApiApplicationTests {
 
     @Test
